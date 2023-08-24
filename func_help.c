8a9a7a4a6e16fde06d4c9b6e0a82b06d84c82e54
RaphSchp <raphaelschopfer@gmail.com>
2023-08-24 14:32:49 +0000

    fix betty style

--- func_help.c
+++ func_help.c
@@ -8,38 +8,38 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-    char *concatenated_string;
-    unsigned int len_s1;
-    unsigned int len_s2;
-    unsigned int index;
+	char *concatenated_string;
+	unsigned int len_s1;
+	unsigned int len_s2;
+	unsigned int index;
 
-    if (s1 == NULL)
-        s1 = "";
+	if (s1 == NULL)
+		s1 = "";
 
-    if (s2 == NULL)
-        s2 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-    for (len_s1 = 0; s1[len_s1] != '\0'; len_s1++)
-        continue;
+	for (len_s1 = 0; s1[len_s1] != '\0'; len_s1++)
+		continue;
 
-    for (len_s2 = 0; s2[len_s2] != '\0'; len_s2++)
-        continue;
+	for (len_s2 = 0; s2[len_s2] != '\0'; len_s2++)
+		continue;
 
-    len_s2 = len_s2 + 1;
+	len_s2 = len_s2 + 1;
 
-    concatenated_string = malloc((len_s1 + len_s2) * sizeof(char));
+	concatenated_string = malloc((len_s1 + len_s2) * sizeof(char));
 
-    if (concatenated_string == NULL)
-        return (0);
+	if (concatenated_string == NULL)
+		return (0);
 
-    for (index = 0; index < (len_s1 + len_s2); index++)
-    {
-        if (index < len_s1)
-            concatenated_string[index] = s1[index];
-        if (index >= len_s1)
-            concatenated_string[index] = s2[index - len_s1];
-    }
-    return (concatenated_string);
+	for (index = 0; index < (len_s1 + len_s2); index++)
+	{
+		if (index < len_s1)
+			concatenated_string[index] = s1[index];
+		if (index >= len_s1)
+			concatenated_string[index] = s2[index - len_s1];
+	}
+	return (concatenated_string);
 }
 
 /**
@@ -51,41 +51,41 @@ char *str_concat(char *s1, char *s2)
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-    char *new_memory;
-    char *original_memory;
-    unsigned int copy_index;
+	char *new_memory;
+	char *original_memory;
+	unsigned int copy_index;
 
-    original_memory = (char *)ptr;
+	original_memory = (char *)ptr;
 
-    if (ptr == NULL)
-        return (malloc(new_size));
+	if (ptr == NULL)
+		return (malloc(new_size));
 
-    if (new_size == 0 && ptr != NULL)
-    {
-        free(ptr);
-        return (NULL);
-    }
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
 
-    if (new_size == old_size)
-        return (ptr);
+	if (new_size == old_size)
+		return (ptr);
 
-    new_memory = malloc(new_size * sizeof(char));
-    if (new_memory == NULL)
-    {
-        free(new_memory);
-        return (NULL);
-    }
+	new_memory = malloc(new_size * sizeof(char));
+	if (new_memory == NULL)
+	{
+		free(new_memory);
+		return (NULL);
+	}
 
-    if (new_size > old_size)
-    {
-        for (copy_index = 0; copy_index < old_size; copy_index++)
-            new_memory[copy_index] = original_memory[copy_index];
-    }
-    if (new_size < old_size)
-    {
-        for (copy_index = 0; copy_index < new_size; copy_index++)
-            new_memory[copy_index] = original_memory[copy_index];
-    }
-    free(original_memory);
-    return (new_memory);
+	if (new_size > old_size)
+	{
+		for (copy_index = 0; copy_index < old_size; copy_index++)
+			new_memory[copy_index] = original_memory[copy_index];
+	}
+	if (new_size < old_size)
+	{
+		for (copy_index = 0; copy_index < new_size; copy_index++)
+			new_memory[copy_index] = original_memory[copy_index];
+	}
+	free(original_memory);
+	return (new_memory);
 }
